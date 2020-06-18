54d9adeed01b0af5fd07946feaac5c03eb295bdc
Gerard Abelló Serras <gerardabello@gmail.com>
2020-06-18 19:00:43 +0200

    feat: Add lexer

---
+++ lexer/src/lib.rs
@@ -0,0 +1,228 @@
+use std::iter::Iterator;
+
+#[derive(PartialEq, Debug)]
+pub enum Token {
+    Identifier { name: String },
+    Integer { string: String },
+    Let,
+    If,
+    Else,
+    Function,
+    Assign,
+    Return,
+
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    Bang,
+
+    True,
+    False,
+
+    Equal,
+    NotEqual,
+    LessThan,
+    GreaterThan,
+    LessThanEqual,
+    GreaterThanEqual,
+
+    OpenParenthesis,
+    CloseParenthesis,
+    OpenBrace,
+    CloseBrace,
+
+    SingleQuote,
+    DoubleQuote,
+
+    Comma,
+    Semicolon,
+}
+
+fn is_whitespace(c: char) -> bool {
+    c.is_whitespace()
+}
+
+fn is_letter(c: char) -> bool {
+    c.is_alphabetic()
+}
+
+fn is_number(c: char) -> bool {
+    c.is_digit(10)
+}
+
+pub struct Lexer<T: Iterator<Item = char>> {
+    iter: T,
+    saved_char: Option<char>,
+}
+
+impl<T: Iterator<Item = char>> Lexer<T> {
+    pub fn new(iter: T) -> Self {
+        Lexer {
+            iter,
+            saved_char: None,
+        }
+    }
+
+    fn next_char(&mut self) -> Option<char> {
+        if let Some(c) = self.saved_char {
+            self.saved_char = None;
+            return Some(c);
+        }
+        if let Some(c) = self.iter.next() {
+            return Some(c);
+        }
+        None
+    }
+
+    fn save_char_for_next_loop(&mut self, c: char) {
+        if !is_whitespace(c) {
+            self.saved_char = Some(c);
+        }
+    }
+
+    fn next_char_skipping_whitespace(&mut self) -> Option<char> {
+        if let Some(c) = self.next_char() {
+            if is_whitespace(c) {
+                return self.next_char_skipping_whitespace();
+            }
+            return Some(c);
+        }
+        None
+    }
+
+    fn next_token_starting_with_equal(&mut self) -> Option<Token> {
+        if let Some(c) = self.next_char() {
+            match c {
+                '=' => Some(Token::Equal),
+                remain => {
+                    self.save_char_for_next_loop(remain);
+                    Some(Token::Assign)
+                }
+            }
+        } else {
+            None
+        }
+    }
+
+    fn next_token_starting_with_bang(&mut self) -> Option<Token> {
+        if let Some(c) = self.next_char() {
+            match c {
+                '=' => Some(Token::NotEqual),
+                remain => {
+                    self.save_char_for_next_loop(remain);
+                    Some(Token::Bang)
+                }
+            }
+        } else {
+            None
+        }
+    }
+
+    fn next_token_starting_with_less_than(&mut self) -> Option<Token> {
+        if let Some(c) = self.next_char() {
+            match c {
+                '=' => Some(Token::LessThanEqual),
+                remain => {
+                    self.save_char_for_next_loop(remain);
+                    Some(Token::LessThan)
+                }
+            }
+        } else {
+            None
+        }
+    }
+
+    fn next_token_starting_with_greater_than(&mut self) -> Option<Token> {
+        if let Some(c) = self.next_char() {
+            match c {
+                '=' => Some(Token::GreaterThanEqual),
+                remain => {
+                    self.save_char_for_next_loop(remain);
+                    Some(Token::GreaterThan)
+                }
+            }
+        } else {
+            None
+        }
+    }
+
+    fn next_number_token(&mut self, first: char) -> Token {
+        let mut string = first.to_string();
+        loop {
+            if let Some(c) = self.next_char() {
+                if is_number(c) {
+                    string.push(c);
+                    continue;
+                } else {
+                    self.save_char_for_next_loop(c);
+                }
+            }
+            break;
+        }
+
+        Token::Integer { string }
+    }
+
+    fn next_word_token(&mut self, first: char) -> Token {
+        let mut string = first.to_string();
+        loop {
+            if let Some(c) = self.next_char() {
+                if is_letter(c) {
+                    string.push(c);
+                    continue;
+                } else {
+                    self.save_char_for_next_loop(c);
+                }
+            }
+            break;
+        }
+
+        match &string[..] {
+            "let" => Token::Let,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "fn" => Token::Function,
+            "return" => Token::Return,
+            "true" => Token::True,
+            "false" => Token::False,
+            _ => Token::Identifier { name: string },
+        }
+    }
+
+    fn next_token(&mut self) -> Option<Token> {
+        if let Some(c) = self.next_char_skipping_whitespace() {
+            return match c {
+                '+' => Some(Token::Plus),
+                '-' => Some(Token::Minus),
+                '*' => Some(Token::Asterisk),
+                '/' => Some(Token::Slash),
+                ';' => Some(Token::Semicolon),
+                '(' => Some(Token::OpenParenthesis),
+                ')' => Some(Token::CloseParenthesis),
+                '{' => Some(Token::OpenBrace),
+                '}' => Some(Token::CloseBrace),
+                ',' => Some(Token::Comma),
+                '"' => Some(Token::DoubleQuote),
+                '\'' => Some(Token::SingleQuote),
+                '!' => self.next_token_starting_with_bang(),
+                '=' => self.next_token_starting_with_equal(),
+                '<' => self.next_token_starting_with_less_than(),
+                '>' => self.next_token_starting_with_greater_than(),
+                c if is_letter(c) => Some(self.next_word_token(c)),
+                c if is_number(c) => Some(self.next_number_token(c)),
+                _ => panic!(format!("Unexpected character '{}'", c)),
+            };
+        }
+
+        None
+    }
+}
+
+impl<T: Iterator<Item = char>> Iterator for Lexer<T> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Token> {
+        self.next_token()
+    }
+}
---
+++ lexer/tests/integration_test.rs
@@ -0,0 +1,146 @@
+use lexer::{Lexer, Token};
+
+fn lex_string(code: &str) -> Vec<Token> {
+    Lexer::new(code.chars()).collect()
+}
+
+#[test]
+fn test_single_char_tokens() {
+
+    let code = "
+        * / !
+        ,
+        ;
+        =
+        {}
+        ()
+        '\"
+        >
+        <
+        ";
+
+    let expected_tokens = vec![
+        Token::Asterisk,
+        Token::Slash,
+        Token::Bang,
+        Token::Comma,
+        Token::Semicolon,
+        Token::Assign,
+        Token::OpenBrace,
+        Token::CloseBrace,
+        Token::OpenParenthesis,
+        Token::CloseParenthesis,
+        Token::SingleQuote,
+        Token::DoubleQuote,
+        Token::GreaterThan,
+        Token::LessThan,
+    ];
+
+    assert_eq!(lex_string(code), expected_tokens);
+
+}
+
+#[test]
+fn test_multiple_char_tokens() {
+
+    let code = "
+        ==
+        !=
+        ! =
+           = =
+           >= <=
+        ";
+
+    let expected_tokens = vec![
+        Token::Equal,
+        Token::NotEqual,
+        Token::Bang,
+        Token::Assign,
+        Token::Assign,
+        Token::Assign,
+        Token::GreaterThanEqual,
+        Token::LessThanEqual,
+    ];
+
+    assert_eq!(lex_string(code), expected_tokens);
+}
+
+#[test]
+fn test_number_tokens() {
+
+    let code = "
+        0 9
+        09
+        50 3336 0005
+        ";
+
+    let expected_tokens = vec![
+        Token::Integer{string: String::from("0")},
+        Token::Integer{string: String::from("9")},
+        Token::Integer{string: String::from("09")},
+        Token::Integer{string: String::from("50")},
+        Token::Integer{string: String::from("3336")},
+        Token::Integer{string: String::from("0005")},
+    ];
+
+    assert_eq!(lex_string(code), expected_tokens);
+}
+
+#[test]
+fn test_word_tokens() {
+
+    let code = "
+            let if else fn return true false
+            Animal dog Cat mandarinA
+        ";
+
+    let expected_tokens = vec![
+        Token::Let,
+        Token::If,
+        Token::Else,
+        Token::Function,
+        Token::Return,
+        Token::True,
+        Token::False,
+        Token::Identifier{name: String::from("Animal")},
+        Token::Identifier{name: String::from("dog")},
+        Token::Identifier{name: String::from("Cat")},
+        Token::Identifier{name: String::from("mandarinA")},
+    ];
+
+    assert_eq!(lex_string(code), expected_tokens);
+}
+
+#[test]
+fn test_code_sum_function() {
+
+    let code = "
+        fn calculation (a, b) {
+          return a + (b-42 );
+        }
+        ";
+
+    let expected_tokens = vec![
+        Token::Function,
+        Token::Identifier { name: String::from("calculation")},
+        Token::OpenParenthesis,
+        Token::Identifier { name: String::from("a")},
+        Token::Comma ,
+        Token::Identifier { name: String::from("b")},
+        Token::CloseParenthesis,
+        Token::OpenBrace,
+        Token::Return,
+        Token::Identifier { name: String::from("a")},
+        Token::Plus,
+        Token::OpenParenthesis,
+        Token::Identifier { name: String::from("b")},
+        Token::Minus,
+        Token::Integer { string: String::from("42")},
+        Token::CloseParenthesis,
+        Token::Semicolon,
+        Token::CloseBrace,
+    ];
+
+    assert_eq!(lex_string(code), expected_tokens);
+
+}
